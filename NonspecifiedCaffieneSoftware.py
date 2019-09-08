61124420c0b425e994cccb902006d73f39189bac
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2019-09-08 00:33:42 -0400

    Update NonspecifiedCaffieneSoftware.py

--- NonspecifiedCaffieneSoftware.py
+++ NonspecifiedCaffieneSoftware.py
@@ -1,1 +1,14 @@
 #Python Script to run when opening a .NCS file
+class caffeineFunctions:
+  class regVarDecl:
+    print(0);
+  class plusPlusPlusETC:
+    print(0);
+  class plusEquals:
+    print(0);
+  class minusEquals:
+    print(0);
+  class timesEquals:
+    print(0);
+  class divideEquals:
+    print(0);
