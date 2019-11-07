83c1c98501a08f330b19f1ecbb936f08cd88cf56
mmmmmk, here's hiddenmonkey <39420031+thehiddenmonkey@users.noreply.github.com>
2019-11-07 15:32:10 -0500

    Update MACfatalError.bash

--- MACfatalError.bash
+++ MACfatalError.bash
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-sudo shutdown -r
+errorCode=$(( $RANDOM % 32767 + 1 ))
+fiftyFifty=$(( $RANDOM % 2 + 1 ))
+fiftyFifty=`expr $fiftyFifty \* 2`
+fiftyFifty=`expr $fiftyFifty - 3`
+errorCode=`expr $fiftyFifty \* $errorCode`
+
+echo FATAL ERROR HAS OCCURRED, ERROR CODE $errorCode
+say 'FATAL ERROR. SHUTDOWN AND DESTRUCTION OF COMPUTER IMMINENT. DELETING ALL FILES...'
+sleep 8
+say 'sudo rm -rf /* activated'
+sleep 4
+say 'SUCCESS! ALL FILES DELETED. DESTROYING COMPUTER'
+sleep 6
+say 'SUCCESS!'
+sleep 2
+osascript -e 'tell app "System Events" to shut down'
